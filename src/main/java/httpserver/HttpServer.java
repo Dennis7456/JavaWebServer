9b39561faa92b6e811162e2f1d34185c15009a1c
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-01-15 01:26:59 +0300

    Basic setup

---
+++ src/main/java/httpserver/HttpServer.java
@@ -0,0 +1,13 @@
+package httpserver;
+
+/**
+ *
+ * Http Server Driver Class
+ *
+ */
+public class HttpServer {
+
+    public static void main(String[] args){
+        System.out.println("Start server...");
+    }
+}
