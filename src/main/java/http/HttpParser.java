8ed052b8f489f50d7a88ecd6facf041904addfec
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-01-15 06:50:21 +0300

    request line validity test cases

--- src/main/java/http/HttpParser.java
+++ src/main/java/http/HttpParser.java
@@ -16,7 +16,7 @@ public class HttpParser {
     private static final int CR = 0x0D; //13 Carriage return
     private static final int LF = 0x0A; //10 Line feed
 
-    public HttpRequest parseHttpRequest(InputStream inputStream) throws IOException {
+    public HttpRequest parseHttpRequest(InputStream inputStream) throws HttpParsingException {
         //Input stream reader to bridge from byte stream to char stream
         InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
 
@@ -28,7 +28,11 @@ public class HttpParser {
          *
          */
         //Start line
-        parseRequestLine(reader, request);
+        try {
+            parseRequestLine(reader, request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         //Header
         parseHeaders(reader,request);
         //Message body
@@ -37,13 +41,49 @@ public class HttpParser {
         return request;
     }
 
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
+    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException, HttpParsingException {
+        StringBuilder processingDataBuffer = new StringBuilder();
+
+        boolean methodParsed = false;
+        boolean requestTargetParsed = false;
+
         int _byte;
         while ((_byte = reader.read()) >= 0){
             if(_byte == CR){
                 _byte = reader.read();
                 if(_byte == LF){
+                    //At the end of the request line we return
+                    LOGGER.debug("Request Line HTTP version to Process : {}" , processingDataBuffer.toString());
+                    //Important to have return statement
+                    if(!methodParsed || !requestTargetParsed){
+                        throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+                    }
                     return;
+                }
+
+            }
+            if(_byte == SP){
+                //check for space character and skip spaces
+                if(!methodParsed){
+                    LOGGER.debug("Request Line METHOD to Process : {}" , processingDataBuffer);
+                    methodParsed = true;
+                    request.setMethod(processingDataBuffer.toString());
+                    //Important to use else if here
+                } else if(!requestTargetParsed) {
+                    LOGGER.debug("Request Line Request Target to Process : {}" , processingDataBuffer);
+                    requestTargetParsed =true;
+                    request.setRequestTarget(processingDataBuffer.toString());
+                } else {
+                    throw new HttpParsingException(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+                }
+                processingDataBuffer.delete(0, processingDataBuffer.length());
+            } else {
+                //if no space character build string
+                processingDataBuffer.append((char) _byte);
+                if(!methodParsed){
+                    if(processingDataBuffer.length() > HttpMethod.MAX_LENGTH){
+                        throw new HttpParsingException(HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+                    }
                 }
             }
         }
--- src/test/java/http/HttpParserTest.java
+++ src/test/java/http/HttpParserTest.java
@@ -22,31 +22,110 @@ class HttpParserTest {
     }
 
     @Test
-    void parseHttpRequest() throws IOException {
-        httpParser.parseHttpRequest(generateValidTestCase());
+    void parseHttpGetRequest() throws IOException {
+        HttpRequest request = null;
+        try {
+            request = httpParser.parseHttpRequest(generateValidGET());
+        } catch (HttpParsingException e) {
+            fail(e);
+        }
+        assertEquals(request.getMethod(), HttpMethod.GET);
     }
 
-    private InputStream generateValidTestCase(){
-        String rawData = "GET / HTTP/1.1\r\n" +
-                "Host: localhost:9000\r\n" +
-                "Connection: keep-alive\r\n" +
-                "Cache-Control: max-age=0\r\n" +
-                "sec-ch-ua: \" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"97\", \"Chromium\";v=\"97\"\r\n" +
-                "sec-ch-ua-mobile: ?0\r\n" +
-                "sec-ch-ua-platform: \"macOS\"\r\n" +
-                "DNT: 1\r\n" +
-                "Upgrade-Insecure-Requests: 1\r\n" +
-                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36\r\n" +
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\n" +
-                "Sec-Fetch-Site: none\r\n" +
-                "Sec-Fetch-Mode: navigate\r\n" +
-                "Sec-Fetch-User: ?1\r\n" +
-                "Sec-Fetch-Dest: document\r\n" +
-                "Accept-Encoding: gzip, deflate, br\r\n" +
-                "Accept-Language: en-US,en;q=0.9,nl;q=0.8,it;q=0.7\r\n" +
-                "\r\n";
+    @Test
+    void parseHttpGetRequestBadMethod() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateInvalidGET());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+
+    }
+
+    @Test
+    void parseHttpGetRequestMethodTooLong() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateInvalidMethodLength());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.SERVER_ERROR_501_NOT_IMPLEMENTED);
+        }
+
+    }
+
+    @Test
+    void parseHttpGetRequestRequestLineInNumOfItems() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateInvalidRequestLineLength());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+    @Test
+    void parseHttpRequestLineEmpty() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateEmptyRequestLine());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+
+    @Test
+    void parseHttpRequestLineNoLineFeed() throws IOException {
+        try {
+            HttpRequest request = httpParser.parseHttpRequest(generateRequestLineCarriageReturnNoLineFeed());
+            fail();
+        } catch (HttpParsingException e) {
+            assertEquals(e.getErrorCode(), HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST);
+        }
+
+    }
+
+    private InputStream generateValidGET(){
+        String rawData = "GET / HTTP/1.1\r\n";
 
         InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
         return inputStream;
     }
+
+    private InputStream generateInvalidGET(){
+        String rawData = "GeT / HTTP/1.1\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateInvalidMethodLength(){
+        String rawData = "GEEEEET / HTTP/1.1\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateInvalidRequestLineLength(){
+        String rawData = "GET / AAAA HTTP/1.1\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateEmptyRequestLine(){
+        String rawData = "\r\n";
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
+    private InputStream generateRequestLineCarriageReturnNoLineFeed(){
+        String rawData = "GET / HTTP/1.1 \r"; //<---- no LF
+
+        InputStream inputStream = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
+        return inputStream;
+    }
+
 }
